e4fae0c1f1de2d04e8bf4fd68a9312a97b8cac42
Denis <corvus007@yandex.ru>
2017-11-21 12:03:53 +0300

    Добавляет функцию getScrollbarWidth в файл util.js

--- app/js/plugins/util.js
+++ app/js/plugins/util.js
@@ -23,6 +23,25 @@ window.util = (function() {
       Array.from(elements).forEach(function specifyMaxHeight(it) {
         it.style.height = maxHeight + 'px';
       });
+    },
+    getScrollbarWidth: function() {
+      // создадим элемент с прокруткой
+      var div = document.createElement('div');
+
+      div.style.overflowY = 'scroll';
+      div.style.width = '50px';
+      div.style.height = '50px';
+
+      // при display:none размеры нельзя узнать
+      // нужно, чтобы элемент был видим,
+      // visibility:hidden - можно, т.к. сохраняет геометрию
+      div.style.visibility = 'hidden';
+
+      document.body.appendChild(div);
+      var scrollWidth = div.offsetWidth - div.clientWidth;
+      document.body.removeChild(div);
+
+      return scrollWidth;
     }
   };
 })();
