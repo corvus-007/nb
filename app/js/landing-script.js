c48bf9378032f3b8924798fb4bd9d90e66f19e3c
Denis <corvus007@yandex.ru>
2018-03-16 11:42:22 +0300

    Добавляет скрипты для общего слайдера тренировок и делает некоторые исправления в файле landing-script.js

--- app/js/landing-script.js
+++ app/js/landing-script.js
@@ -1,14 +1,27 @@
-$(function() {
+$(function () {
   $.fancybox.defaults.animationEffect = 'zoom-in-out';
 
   var partnersSlider = document.querySelector('.landing-partners-slider');
   var landingCoachesSlider = document.querySelector('.landing-coaches-slider');
   var landingMembersSlider = document.querySelector('.landing-members-slider');
+  var landingTrainingSlider = document.querySelector('.js-landing-training-slider');
+  var memberProfileSlider = document.querySelector('.js-member-profile-slider');
+  var currentSlide;
+  var slidesCount;
 
-  $('[type="tel"]').mask('+7 (999) 999-99-99', { autoclear: false });
+  var updateSliderCounter = function (slick, sliderCounter, currentIndex) {
+    currentSlide = slick.slickCurrentSlide() + 1;
+    slidesCount = slick.slideCount;
+    $(sliderCounter).text(currentSlide + '/' + slidesCount)
+  };
+
+  $('[type="tel"]').mask('+7 (999) 999-99-99', {
+    autoclear: false
+  });
+
   $.validator.addMethod(
     'customphone',
-    function(value, element) {
+    function (value, element) {
       return (
         this.optional(element) ||
         /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/.test(value)
@@ -18,14 +31,14 @@ $(function() {
   );
 
   $('#fitness-request-form').validate({
-    submitHandler: function(form) {
+    submitHandler: function (form) {
       var data = $(form).serialize();
       $.ajax({
         url: '/index.php?v[mode]=specproject&v[action]=fitnes2018reg',
         async: false,
         data: data,
         method: 'POST',
-        success: function(result) {
+        success: function (result) {
           if (result['status'] == '1') {
             $.fancybox.close(true);
             $.fancybox.open(
@@ -65,8 +78,7 @@ $(function() {
       centerPadding: '70px',
       slidesToShow: 1,
       variableWidth: true,
-      responsive: [
-        {
+      responsive: [{
           breakpoint: 767,
           settings: 'unslick'
         },
@@ -90,8 +102,7 @@ $(function() {
       centerMode: true,
       mobileFirst: true,
       slidesToShow: 1,
-      responsive: [
-        {
+      responsive: [{
           breakpoint: 1023,
           settings: 'unslick'
         },
@@ -113,8 +124,7 @@ $(function() {
       centerMode: true,
       mobileFirst: true,
       slidesToShow: 1,
-      responsive: [
-        {
+      responsive: [{
           breakpoint: 1023,
           settings: 'unslick'
         },
@@ -124,6 +134,50 @@ $(function() {
             centerMode: true,
             slidesToShow: 1,
             variableWidth: true
+          }
+        }
+      ]
+    });
+  }
+
+  if (landingTrainingSlider) {
+    var sliderCounterTraining = document.createElement('div');
+    sliderCounterTraining.classList.add('landing-training-slider__counter');
+
+    $(landingTrainingSlider).on('init', function (event, slick) {
+      $(landingTrainingSlider).append(sliderCounterTraining);
+      updateSliderCounter(slick, sliderCounterTraining);
+    });
+
+    $(landingTrainingSlider).on('afterChange', function (event, slick, currentSlide) {
+      updateSliderCounter(slick, sliderCounterTraining, currentSlide);
+    });
+
+    $(landingTrainingSlider).slick({
+      accessibility: false,
+      autoplay: true,
+      centerMode: true,
+      slidesToShow: 3,
+      centerPadding: 0,
+      variableWidth: true,
+      speed: 900,
+      appendArrows: '.landing-training-slider__arrows-holder',
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 1,
+            centerMode: false,
+            variableWidth: false
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            arrows: false,
+            slidesToShow: 1,
+            variableWidth: false,
+            centerMode: false
           }
         }
       ]
@@ -146,20 +200,28 @@ $(function() {
     });
   }
 
-  var $memberProfileSlider = $('.js-member-profile-slider');
+  if (memberProfileSlider) {
+    var sliderCounterProfileTraining = document.createElement('div');
+    sliderCounterProfileTraining.classList.add('member-profile-slider__counter');
 
-  if ($memberProfileSlider.length) {
-    $memberProfileSlider.slick({
+    $(memberProfileSlider).on('init', function (event, slick) {
+      $(memberProfileSlider).append(sliderCounterProfileTraining);
+      updateSliderCounter(slick, sliderCounterProfileTraining);
+    });
+
+    $(memberProfileSlider).on('afterChange', function (event, slick, currentSlide) {
+      updateSliderCounter(slick, sliderCounterProfileTraining, currentSlide);
+    });
+
+    $(memberProfileSlider).slick({
       accessibility: false,
-      dots: true,
       centerMode: true,
       slidesToShow: 3,
       centerPadding: 0,
       variableWidth: true,
       speed: 900,
       appendArrows: '.member-profile-slider__arrows-holder',
-      responsive: [
-        {
+      responsive: [{
           breakpoint: 768,
           settings: {
             arrows: false,
